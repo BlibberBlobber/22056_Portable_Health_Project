08477dd1a87f9a29ac81aa26032d6ec1f1432b2b
Morten Vorborg <mortenvorborg@hotmail.com>
2021-03-18 09:05:56 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/a22056_app/MainActivity.java
+++ app/src/main/java/com/example/a22056_app/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Hello my friends how are you doing?
+        // Hello my friends how are you doing? endnu en test
 
 
     }
