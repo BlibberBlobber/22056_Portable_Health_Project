0fc0a89ec205c2faad8ab063cedc746e17cc6783
oliverWiik <54854942+oliverWiik@users.noreply.github.com>
2021-04-06 16:17:20 +0200

    Update mainDSP.m

--- MATLAB/mainDSP.m
+++ MATLAB/mainDSP.m
@@ -9,56 +9,16 @@ addpath(genpath(fullfile(root,'Data')))
 clear root
 
 dataFolderList=dir("Data");
-dataFolderPath = pwd + "/Data/" + dataFolderList(4).name; % The number should be 3:end and notes the folders containing data
-
-[fileDataCell, dataFolderPath] = readAllCsvFromFolder(dataFolderPath); % Optional input of folder path
-
-modalityFieldNames = string(zeros(1,size(fileDataCell,1)));
-for fileIdx = 1:size(fileDataCell,1)
-    modalityFieldNames(fileIdx) = convertCharsToStrings(fileDataCell{fileIdx,2}(1:end-4));
+if ispc()
+    dataFolderPath = pwd + "\Data\" + dataFolderList(4).name; % The number should be 3:end and notes the folders containing data
+    [fileDataCell, dataFolderPath] = readAllCsvFromFolder(dataFolderPath); % Optional input of folder path
+elseif ismac()
+    dataFolderPath = pwd + "/Data/" + dataFolderList(4).name; % The number should be 3:end and notes the folders containing data
+    [fileDataCell, dataFolderPath] = readAllCsvFromFolder(dataFolderPath); % Optional input of folder path
+else
+    [fileDataCell, dataFolderPath] = readAllCsvFromFolder(); % Optional input of folder path
 end
 
-for i = 1:size(fileDataCell,1)
-    data = fileDataCell{i,1};
-    if ~isempty(data) % A must check - "tags" is empty and causes errors
-        
-        SerialDateUTC = string(datestr(data(1,1).Variables/86400 + datenum(1970,1,1)));
-        time = datetime(SerialDateUTC,"Format","dd-MMM-uuuu HH:mm:ss.SSSSSS");
-        
-        if isequal(modalityFieldNames(i),"ACC")
-            % Handle ACC
-            
-            fs = data(2,1).Variables;
-            sampleDuration = seconds(1/fs);
-            
-            data([1,2],:) = [];
-            
-            tt = time+sampleDuration:sampleDuration:time+sampleDuration*size(data,1);
-            
-            data.time = tt';
-            data.Properties.VariableNames = {'x' 'y' 'z','time'};
-            
-        elseif isequal(modalityFieldNames(i),"IBI")
-            % Handle IBI
-            tt = time+seconds(data(2:end,1).Variables);
-            data([1],:) = [];
-            data.Var1 = tt;
-            data.Properties.VariableNames = {'time','amplitude'};
-        else
-            % Handle other modalities
-            fs = data(2,1).Variables;
-            sampleDuration = seconds(1/fs);
-            
-            data([1,2],:) = [];
-            tt = time+sampleDuration:sampleDuration:time+sampleDuration*size(data,1);
-            data = table(tt,data.Variables');
-            data.Properties.VariableNames = {'time','amplitude'};
-        end
-    end
-    
-    fileDataCell{i,1} = data;
-    
-end
 
 %% Plot E4 data
 
