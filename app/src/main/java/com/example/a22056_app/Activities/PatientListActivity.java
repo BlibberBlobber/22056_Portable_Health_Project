c34e3ea94fb3032dc7b38af2fcd1eab23a3f4aaa
danielhanove <44897299+danielhanove@users.noreply.github.com>
2021-04-07 17:02:17 +0200

    "Minor change"

--- app/src/main/java/com/example/a22056_app/Activities/PatientListActivity.java
+++ app/src/main/java/com/example/a22056_app/Activities/PatientListActivity.java
@@ -10,6 +10,6 @@ public class PatientListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_patient_list2);
+        setContentView(R.layout.activity_patient_list);
     }
 }
