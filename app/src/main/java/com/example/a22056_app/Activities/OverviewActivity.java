bbf70e9484566619ab4fdf58e094e1bd157a17af
danielhanove <44897299+danielhanove@users.noreply.github.com>
2021-04-22 10:55:32 +0200

    Revert "Merge branch 'master' into Daniel"
    This reverts commit b1c80c329e182f0e65dad0a6185b5fa8939216f9, reversing
    changes made to 6e31423513fe71c08cba8a21c547d071a1817bc1.

---
+++ app/src/main/java/com/example/a22056_app/Activities/OverviewActivity.java
@@ -0,0 +1,29 @@
+package com.example.a22056_app.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.a22056_app.R;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    Button measurementsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+        measurementsButton = findViewById(R.id.measurementsButton);
+        measurementsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OverviewActivity.this, MeasurementsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
