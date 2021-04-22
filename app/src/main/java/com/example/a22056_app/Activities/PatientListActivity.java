e23fb3aa22fcd570d0128f793c50893dbda0a16e
danielhanove <44897299+danielhanove@users.noreply.github.com>
2021-04-22 10:46:51 +0200

    Revert ""A few changes here and there""
    This reverts commit 8777e2ca6be8f67c2d862673a45aa00baf171fb1.

--- app/src/main/java/com/example/a22056_app/Activities/PatientListActivity.java
+++ app/src/main/java/com/example/a22056_app/Activities/PatientListActivity.java
@@ -1,79 +1,16 @@
 package com.example.a22056_app.Activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.Toolbar;
 
-import com.example.a22056_app.Models.Patient;
-import com.example.a22056_app.PatientListAdapter;
 import com.example.a22056_app.R;
-import com.example.a22056_app.Tools.DatabaseHandler;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
 
 public class PatientListActivity extends AppCompatActivity {
-
-    private ListView listView;
-    private Button addPatientButton;
-    private PatientListAdapter listAdapter;
-    Toolbar toolbar;
-    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_list);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        listView = findViewById(R.id.patientListView);
-        progressBar = findViewById(R.id.progressBar2);
-        addPatientButton = findViewById(R.id.addButton);
-        addPatientButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(PatientListActivity.this, AddPatientActivity.class);
-                startActivity(intent);
-            }
-        });
-        progressBar.setVisibility(View.VISIBLE);
-        DatabaseHandler.getCollection("users", new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                progressBar.setVisibility(View.INVISIBLE);
-                if (!task.isSuccessful()){
-                    Log.d("Firestore", "Error getting data " + task.getException().getMessage());
-                }else {
-                    ArrayList<Patient> patients = new ArrayList<>();
-                    for (QueryDocumentSnapshot document: task.getResult()){
-                        Log.d("Firestore",document.getId() + " => " + document.getData());
-                        Patient patient = new Patient(document.getData());
-                        patients.add(patient);
-                    }
-                    setListAdapter(patients);
-                }
-            }
-        });
-    }
-
-    private void setSupportActionBar(Toolbar toolbar) {
-    }
-
-    private void setListAdapter(ArrayList<Patient> patients){
-
-        listAdapter = new PatientListAdapter(this, patients);
-        listView.setAdapter(listAdapter);
-
     }
 }
--- app/src/main/java/com/example/a22056_app/Tools/DatabaseHandler.java
+++ app/src/main/java/com/example/a22056_app/Tools/DatabaseHandler.java
@@ -1,52 +1,40 @@
 package com.example.a22056_app.Tools;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.example.a22056_app.Activities.LoginActivity;
 import com.example.a22056_app.Activities.SignUpActivity;
-import com.example.a22056_app.Models.Patient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 
 public class DatabaseHandler {
 
-    private static FirebaseAuth mmAuth;
-    //private static FirebaseFirestore firestore;
+    private static FirebaseAuth mAuth;
+    private static FirebaseFirestore firestore;
 
     public DatabaseHandler(){
-        mmAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
     }
     public void login(String username, String password, Context ctx){
-        mmAuth = FirebaseAuth.getInstance();
-        mmAuth.signInWithEmailAndPassword(username, password)
+        mAuth.signInWithEmailAndPassword(username, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         WeakReference<Context> contextWeakReference = new WeakReference<>(ctx);
                         if (contextWeakReference.get() == null){return;}
-                        ((LoginActivity) contextWeakReference.get()).userFromLogin(task.isSuccessful(), mmAuth.getCurrentUser());
+                        ((LoginActivity) contextWeakReference.get()).userFromLogin(task.isSuccessful(), mAuth.getCurrentUser());
                     }
                 });
     }
-    public static void loginTest(String username, String password, OnCompleteListener<AuthResult> onCompleteListener){
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        mAuth.signInWithEmailAndPassword(username, password)
-                .addOnCompleteListener(onCompleteListener);
-    }
     public void createUser(String username, String password, Context ctx){
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
         mAuth.createUserWithEmailAndPassword(username, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
@@ -56,15 +44,6 @@ public class DatabaseHandler {
                         ((SignUpActivity) contextWeakReference.get()).signUpCompletion(task.isSuccessful(), mAuth.getCurrentUser());
                     }
                 });
-    }
-    public static void getCollection(String collectionName, OnCompleteListener<QuerySnapshot> onCompleteListener){
-        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-        firestore.collection(collectionName).get().addOnCompleteListener(onCompleteListener);
-    }
-
-    public static FirebaseUser getCurrentUser(){
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        return mAuth.getCurrentUser();
     }
 
     public void getData(){}
