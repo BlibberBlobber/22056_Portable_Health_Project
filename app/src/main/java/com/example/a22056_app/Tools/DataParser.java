dcd45930d0167f936d2c31d56064e1321a83c2da
danielhanove <44897299+danielhanove@users.noreply.github.com>
2021-04-23 15:01:36 +0200

    "Class to read csv data"

---
+++ app/src/main/java/com/example/a22056_app/Tools/DataParser.java
@@ -0,0 +1,51 @@
+package com.example.a22056_app.Tools;
+
+import android.util.Log;
+
+import com.example.a22056_app.R;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class DataParser {
+
+    InputStream inputStream;
+
+    public void getData(InputStream inputStream) throws IOException { //not void -> ArrayList<[]double>
+
+
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            CSVReader reader = new CSVReader(inputStreamReader);
+            reader.getLinesRead();
+            String[] nextLine;
+            ArrayList<double[]> valueArray = new ArrayList<>();
+            int counter = 0;
+            while ((nextLine = reader.readNext()) != null) {
+                double[] vals = new double[nextLine.length];
+                for (int i = 0; i < nextLine.length; i++){
+                    double dVal = Double.parseDouble(nextLine[i]);
+                    vals[i] = dVal;
+                }
+                valueArray.add(vals);
+                counter ++;
+            }
+            Log.d("DATAREADER", "Size of data array " + valueArray.size());
+            Log.d("DATAREADER", "Sample from arraylist " + valueArray.get(10)[0]);
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+}
