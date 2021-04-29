22e6e72a6962138d6c53fc067a176cc5ecba1e89
oliverWiik <54854942+oliverWiik@users.noreply.github.com>
2021-04-29 14:57:33 +0200

    A million changes to the processing

--- MATLAB/segmentDataFromProtocol.m
+++ MATLAB/segmentDataFromProtocol.m
@@ -1,33 +1,48 @@
-function [fileDataCell] = segmentDataFromProtocol(fileDataCell, START, END, ORDER, fs)
+function [fileDataCell, classes] = segmentDataFromProtocol(fileDataCell, START, END, ORDER)
 warning('off','all');
 
-for i = 1:size(fileDataCell,1)
-    %if ~isequal(fileDataCell{1,2},"IBI.csv")
-    if ~isnan(fs(i))
-        
-        data = fileDataCell{i};
-        classList = nan(max(size(data.time)),1);
-        stressNoStress = zeros(max(size(data.time)),1);
-        
-        for l = 1:length(ORDER)
-            samplesOfCurrentProcess = floor((END(l) - START(l))*60*fs(i));
-            start = START(l)*60*fs(i);
-            classList(start:start+samplesOfCurrentProcess) = l;
-            
-            if find(ORDER=="TSST")
-                stressNoStress(start:start+samplesOfCurrentProcess) = 1;
-            end
-        end
-    else
-        continue;
-    end
+% stressNoStress = zeros(max(size(data.time)),1);
+
+classes = ["Base" "TSST" "Fun" "Medi 1" "Medi 2" "sRead" "bRead" "fRead"];
+classList = nan(size(fileDataCell{1,1}.time));
+for stateIdx = 1:length(END)
     
-    data = addvars(data,classList,'NewVariableNames','Process');
-    data = addvars(data,stressNoStress,'NewVariableNames','Stress');
-    
-    fileDataCell{i} = data;
-    
+    classList(isbetween(fileDataCell{1,1}.time,...
+        fileDataCell{1,1}.time(1)+minutes(START(stateIdx)),...
+        fileDataCell{1,1}.time(1)+minutes(END(stateIdx)))) = find(classes == ORDER(stateIdx));
 end
+
+fileDataCell{1,1} = [fileDataCell{1,1}, table(classList, 'VariableNames', {'Process'})];
+
+
+
+% for i = 1:size(fileDataCell,1)
+%     %if ~isequal(fileDataCell{1,2},"IBI.csv")
+%     if ~isnan(fs(i))
+%         
+%         data = fileDataCell{i};
+%         classList = nan(max(size(data.time)),1);
+%         stressNoStress = zeros(max(size(data.time)),1);
+%         
+%         for l = 1:length(ORDER)
+%             samplesOfCurrentProcess = floor((END(l) - START(l))*60*fs(i));
+%             start = START(l)*60*fs(i);
+%             classList(start:start+samplesOfCurrentProcess) = l;
+%             
+%             if ORDER(l)=="TSST"
+%                 stressNoStress(start:start+samplesOfCurrentProcess) = 1;
+%             end
+%         end
+%     else
+%         continue;
+%     end
+%     
+%     data = addvars(data,classList,'NewVariableNames','Process');
+%     data = addvars(data,stressNoStress,'NewVariableNames','Stress');
+%     
+%     fileDataCell{i} = data;
+%     
+% end
 
 
 warning('on','all');
