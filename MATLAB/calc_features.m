2b43b6d4e3f13882a91273fe8898df2d2a163835
oliverWiik <54854942+oliverWiik@users.noreply.github.com>
2021-04-29 13:50:01 +0200

    A million changes to the processing

--- MATLAB/calc_features.m
+++ MATLAB/calc_features.m
@@ -1,10 +1,12 @@
 function [features] = calc_features(raw, window_size, feature_funcs)
 % feature_funcs is a cell array of size(num_features, 1)
+winFracOverlap = 240;
 
-features = zeros(floor(length(raw)/window_size), size(feature_funcs,2));
-for idxWin = 0:floor(length(raw)/window_size)-1 % Window loop
+nbOfWin = floor((length(raw)*winFracOverlap/window_size) - (winFracOverlap-1));
+features = zeros(nbOfWin, size(feature_funcs,2));
+for idxWin = 0:nbOfWin-1 % Window loop
     
-    window = raw(idxWin*window_size+1:(idxWin+1)*window_size);
+    window = raw(idxWin*(window_size/winFracOverlap)+1 : idxWin*(window_size/winFracOverlap) + window_size);
     
 
     for idxFeat = 1:size(feature_funcs,2) % Feature loop
